0f2bb794d54fd17e20c609504c55795e89eedbc8
aravindCommits <aravind@Aravinds-Mac-mini.local>
2017-09-04 18:52:12 +0530

    CoreML Sample

--- CoreMLDetecion/ViewController.swift
+++ CoreMLDetecion/ViewController.swift
@@ -7,19 +7,117 @@
 //
 
 import UIKit
+import CoreML
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let imageFinderModel = MobileNet()
+    let imagePicker = UIImagePickerController()
+    
+    @IBOutlet weak var pickedImageView: UIImageView!
+    @IBOutlet weak var imageDescription: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.configureImagePicker()
+        imageDescription.text = ""
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func configureImagePicker() {
+        imagePicker.delegate = self
     }
+    
+    @IBAction func pickImageButtonTapped(_ sender: Any) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    //MARK: Process Image
+    
+    func processImage(_ image: UIImage) {
+        pickedImageView.contentMode = .scaleAspectFit
+        pickedImageView.image = image
+        let imageDiscription = readImage(image)
+        imageDescription.text = "Image Shown is : " + imageDiscription
+    }
+    
+    func readImage(_ image: UIImage) -> String {
+        guard let imageBuffer = image.resize(to: CGSize(width: 224, height: 224)).pixelBuffer() else {
+            fatalError()
+        }
+        guard let imageDescription = try? imageFinderModel.prediction(image: imageBuffer) else {
+            fatalError("Unexpected runtime error.")
+        }
+        
+        return imageDescription.classLabel
+    }
+    
+    //MARK: UIImagePickerControllerDelegate
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            processImage(pickedImage)
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+}
 
+//MARK: Image Resize
 
+extension UIImage {
+    
+    func resize(to newSize: CGSize) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: newSize.width, height: newSize.height), true, 1.0)
+        self.draw(in: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
+        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        return resizedImage
+    }
+    
+    func pixelBuffer() -> CVPixelBuffer? {
+        let width = self.size.width
+        let height = self.size.height
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue,
+                     kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer: CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault,
+                                         Int(width),
+                                         Int(height),
+                                         kCVPixelFormatType_32ARGB,
+                                         attrs,
+                                         &pixelBuffer)
+        
+        guard let resultPixelBuffer = pixelBuffer, status == kCVReturnSuccess else {
+            return nil
+        }
+        
+        CVPixelBufferLockBaseAddress(resultPixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(resultPixelBuffer)
+        
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        guard let context = CGContext(data: pixelData,
+                                      width: Int(width),
+                                      height: Int(height),
+                                      bitsPerComponent: 8,
+                                      bytesPerRow: CVPixelBufferGetBytesPerRow(resultPixelBuffer),
+                                      space: rgbColorSpace,
+                                      bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) else {
+                                        return nil
+        }
+        
+        context.translateBy(x: 0, y: height)
+        context.scaleBy(x: 1.0, y: -1.0)
+        
+        UIGraphicsPushContext(context)
+        self.draw(in: CGRect(x: 0, y: 0, width: width, height: height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(resultPixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
+        
+        return resultPixelBuffer
+    }
 }
 
